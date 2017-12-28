9d58e27ba11c92c8285599a72e81926b8b3b8ff5
HuangHuaBingZiGe <yuezhen_wu@163.com>
2017-12-28 21:16:33 +0800

    20171228

---
+++ JavaWebDemo/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class
+ *
+ * @author Bingzi
+ * @date 2017/12/28
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
